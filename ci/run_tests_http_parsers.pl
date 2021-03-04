a115d2944fbf2d6c85a31560d3bcfb528c1e51b0
harsath <harsath@tuta.io>
2021-03-04 03:38:45 -0500

    gtest added, ci config files&scripts. moved stuff around

---
+++ ci/run_tests_http_parsers.pl
@@ -0,0 +1,14 @@
+#!/bin/perl
+use warnings;
+use strict;
+
+if($#ARGV+1 != 1){ print "Usage: ./script.pl <BUILD-DIR-NAME>"; exit(1); }
+my $build_dir_name = $ARGV[0];
+sub run_tests {
+	if(-d $build_dir_name){ print "[ERROR]: Build dir already exists, remove first"; exit(1); }
+	mkdir($build_dir_name);
+	chdir($build_dir_name);
+	system(qq/cmake -GNinja .. && ninja/);
+}
+
+run_tests();
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 ### Plain-Old-C HTTP parser
+![Linux Actions Status](https://github.com/harsath/poc-http-parser/workflows/Linux/badge.svg)
+
 Originally implemented for my internal use to play around with Linux and FreeBSD's networking interface for event-driven
 APIs, but I find myself using it ofter.
 It's a header-only C HTTP parser. You can just plug-in anywhere.
